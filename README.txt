998eeb0b51bd28815a7dd6731d17b6e5aac4f5d6
Jim Bisso <jim@datastax.com>
2019-12-06 09:31:21 -0800

    First pass at pared-down version of DS templates/build system.

--- README.txt
+++ README.txt
@@ -1,13 +1,27 @@
 Oxygen example
 ==============
 
+We are trying to get our build system to work with the new Oxygen WebHelp
+plug-in. We are using the new page tempaltes, and have added a call to 
+our requirejs module(s). Although we can access the jquery module, we
+cannot access the jquery-ui module. (We are creating some modal dialogs
+to show tables with syntax information for the users.)
+
+I have factored out all extrnenous stuff on our end. We have assets that are
+not in the oxygen-webhelp dirs od each document (we have quite a few docs, and
+this allows us to update assets (css overrides, Javascript, etc.) without
+hacving to rebuild the entire doc set.
+
 Prerequisites
 -------------
 
     1. Set up DITA-OT 3.3.1 on your system.
-    2. Add Oxygen WbHelp plugins.
-    3. Run integrator for DITA-OT.
-    4. Make sure you have a license from OxygenXML.
+    2. Add Oxygen WebHelp 21.1 plugins.
+    3. Make sure you have a license from OxygenXML.
+    4. Run integrator for DITA-OT.
+    5. Set some env variables:
+       a. export DITA_DIR=/path/to/dita_ot-3.3.1
+       b. export WEB_DOCS_ROOT=/path/to/httpd-docs-root
     
 Building document
 -----------------
@@ -17,3 +31,11 @@ Building document
     2. $ $DITA_DIR/bin/dita \
             -Dwebhelp.publishing.template=template/oxygen/oxygen-tree.opt \
             --format=webhelp-responsive --input=src/oxygen_example.ditamap --temp=temp --output=out
+
+    3. cp -r oxygen-example/web $WEB_DOCS_ROOT
+    
+    4. cp -r oxygen-example/out $WEB_DOCS_ROOT
+    
+    5. Open document in browser:
+    
+        URL: http://localhost/out
--- web/dsInit.js
+++ web/dsInit.js
@@ -2,27 +2,68 @@
 
 require.config({
     paths: {
-        "fancybox" : "/webs/jquery.fancybox.min"
-        //"jquery.swiftype.search" : "/en/assets/js/jquery.swiftype.search",
-        //"jquery.swiftype.autocomplete" : "/en/assets/js/jquery.swiftype.autocomplete",
-        //"highlight.pack": "/en/assets/js/highlight.pack",
-        //"navlinks" : "/en/assets/js/navlinks",
-        //"versionDropdown" : "/en/assets/js/versionDropdown"
+        "fancybox" : "/web/jquery.fancybox.min"
     }
 });
 
 var modules = [
     "fancybox"
-    //"jquery.swiftype.search",
-    //"jquery.swiftype.autocomplete",
-    //"highlight.pack",
-    //"navlinks",
-    //"versionDropdown"];
+];
 
 define(modules, function() {
     // initialize the DataStax environment after all required dependcies have loaded
     function dsInit() {
-        console.log("dsInit() called: " + jQuery.ui);
+        console.log("dsInit() called");
+        findModals();
+    }
+    // process modal dialog output
+    function findModals() {
+        console.log("findModals() called");
+        $('.legend').each(function() {
+            var modalOpen = document.createElement('p');
+            var openLink = document.createElement('a');
+            openLink.className += " open-modal";
+            openLink.setAttribute("href", "#");
+            openLink.setAttribute("id", "showLegend");
+            openLink.innerHTML = "Show syntax legend";
+            modalOpen.appendChild(openLink);
+            $(this).before(modalOpen);
+            $(this).wrapAll('<div class="modal-legend" />');
+        });
+        
+        // on clicking the trigger the modal dialog will popup with an overlay
+        $('.open-modal').click(function(event) {
+            $('.modal-legend').dialog("open");
+            event.preventDefault();
+        });
+        
+        // on clicking the close element the modal dialog & overlay will disappear
+        $('.modal-close').click(function(event) {
+            $('.modal-legend, .modal-overlay').hide();
+            event.preventDefault();
+        });
+        
+        // make the modal a dialog
+        if (document.querySelector(".modal-legend") !== null) {
+            try {
+                $('.modal-legend').dialog({
+                    modal: true,
+                    fluid: true,
+                    autoOpen: false,
+                    minHeight: 400,
+                    height: 400,
+                    width: 'auto',
+                    maxWidth: 700,
+                    position: {
+                        my: "left top",
+                        at: "top",
+                        of: "#showLegend"
+                    }
+                });
+            } catch(error) {
+                console.error('findModals error: ' + error.message)
+            }
+        }
     }
     
     // Return the dsInit function so that it can be called from the HTML page
